800bd25bd78affdd3338f12657247fef07bc4074
VoX <icopy11@gmail.com>
2014-11-08 16:49:42 -0500

    Fixed rendering only occuring when a event happened, added .gitignore

--- de/main.py
+++ de/main.py
@@ -19,6 +19,7 @@ mouse_y = 0
 # 0 = impassable, for now
 passable_tiles = map.get_passable_tiles(False)
 main_board = map.generate_map()
+highlightBox = (None,None)
 
 def terminate():
 	pygame.quit()
@@ -46,12 +47,19 @@ while True:
 		tile_x, tile_y = screen.get_tile_at_pixel(mouse_x, mouse_y)
 		if tile_x != None and tile_y != None:
 			# the mouse is over a box
-			screen.draw_highlight_box(tile_x, tile_y)
+			highlightBox = (tile_x, tile_y)
+
 			if not passable_tiles[tile_x][tile_y] and mouse_click:
 				passable_tiles[tile_x][tile_y] = True # set tile as passable
 			elif passable_tiles[tile_x][tile_y] and mouse_click:
 				passable_tiles[tile_x][tile_y] = False
-				
-		# redraw screen
-		pygame.display.update()
-		FPS_clock.tick(FPS)
+		else:
+			highlightBox = (None,None)
+
+	#draw the highlight box if anything is highlighed
+	if highlightBox[0] != None and highlightBox[1] != None:
+		screen.draw_highlight_box(highlightBox[0], highlightBox[1])
+
+	# redraw screen
+	pygame.display.update()
+	FPS_clock.tick(FPS)
